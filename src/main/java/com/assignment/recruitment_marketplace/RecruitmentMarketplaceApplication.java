9401e7f0337cc76d6d90c2363e65d05e19bb3c03
jyotsna-kushwaha <jyotsnakushwaha04121997@gmail.com>
2025-09-23 12:06:39 +0530

    created project using spring initializer

---
+++ src/main/java/com/assignment/recruitment_marketplace/RecruitmentMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.recruitment_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentMarketplaceApplication.class, args);
+	}
+
+}
